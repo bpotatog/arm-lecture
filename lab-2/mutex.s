48920629fae9146ede77337e37bd11ff390f6a9e
bpotatog <basketscott81223@gmail.com>
2014-09-26 17:39:16 +0800

    add lab1 asm code and test data & add lab2 asm code and reduce for loop time

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,13 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+	ldr r1, =locked
+.L1:
+	ldrex r2, [r0]
+	cmp r2, #0
+		strexeq r2, r1, [r0]
+		cmpeq r2, #0
+		bne .L1
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +25,12 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+        ldr r1, =unlocked
+@.L2:
+	@strex r3, r1, [r0]
+	str r1, [r0]
+	@	cmpeq r3, #0
+	@	bne .L2
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
