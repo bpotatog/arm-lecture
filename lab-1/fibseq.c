63624da7d8cbf6500115ed5250799387fe9d0e35
bpotatog <basketscott81223@gmail.com>
2014-10-04 22:28:05 +0800

    lab-1 add fib non-recursive version

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 extern int fibonacci(int x);
+extern int fibonacci_non(int x);
 
 int main(int argc, char **argv)
 {
@@ -8,7 +9,7 @@ int main(int argc, char **argv)
   int result=0;
 
   scanf("%d",&number);
-  result = fibonacci(number);   
+  result = fibonacci_non(number);   
   printf("The fibonacci sequence at %d is: %d\n", number, result);
 }
 
